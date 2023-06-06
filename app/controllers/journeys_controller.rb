2ab945e3e1cb2736b95deddaa0929ac7a2557e00
Vnt0n <antoine.chosson@gmail.com>
2023-06-06 17:00:58 +0200

    navbar cl_tag ok

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -2,18 +2,8 @@ class JourneysController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    @journeys = Journey.all
   end
 
   def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def update
   end
 end
