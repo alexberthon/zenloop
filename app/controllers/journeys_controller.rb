044283260257d2b6462726f06370027f32bc9270
Vnt0n <antoine.chosson@gmail.com>
2023-06-06 15:43:57 +0200

    Little bit styling forms

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,7 +1,19 @@
 class JourneysController < ApplicationController
+  # before_action :authenticate_user!
+
   def index
+    @journeys = Journey.all
   end
 
   def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
   end
 end
