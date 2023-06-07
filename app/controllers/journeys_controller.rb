2d1c8bb5461f1f4510002fcedc044b5df18547a4
Vnt0n <antoine.chosson@gmail.com>
2023-06-07 21:58:21 +0200

    first index step

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -3,22 +3,27 @@ class JourneysController < ApplicationController
   def index
     @search = params["search"]
     @stations = Station.all
-
-    if @search.present?
-      @name = @search["name"]
-      @querystation = Station.find_by(name: @name)
-      @selected_station = Station.where(["id = #{@querystation.id}"])
-    end
-
-    @markers = @selected_station.map do |station|
-      {
-        lat: station.latitude,
-        lng: station.longitude
-      }
-    end
+    @journeys = Journey.all
+    @steps = Step.all
+    @cities = City.all
   end
+
+    # if @search.present?
+    #   @name = @search["name"]
+    #   @querystation = Station.find_by(name: @name)
+    #   @selected_station = Station.where(["id = #{@querystation.id}"])
+    # end
+
+    # @markers = @selected_station.map do |station|
+    #   {
+    #     lat: station.latitude,
+    #     lng: station.longitude
+    #   }
+    # end
 
   def show
     @station = Station.find(params[:id])
+    @journey = Journey.find(params[:id])
   end
+
 end
