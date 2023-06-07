ac2dddf8b332ee33487892d8c3ec13efda759cc1
alexis nurdin <alexisns@proton.me>
2023-06-07 10:50:17 +0200

    fix journey controller

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,15 +1,14 @@
 class JourneysController < ApplicationController
   before_action :authenticate_user!
   def index
+    @search = params["search"]
     @stations = Station.all
-
 
     if @search.present?
       @name = @search["name"]
       @querystation = Station.find_by(name: @name)
       @selected_station = Station.where(["id = #{@querystation.id}"])
     end
-
 
     @markers = @selected_station.map do |station|
       {
