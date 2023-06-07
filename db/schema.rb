e116590289bba26fb17342c9a89838108f11660a
Alexandre Berthon <alexandre_berthon@hotmail.fr>
2023-06-07 17:07:50 +0200

    add db_trip_id to lines and destroy journeys after user destroy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_091339) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_141322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,6 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_091339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "duration"
+    t.string "db_trip_id"
     t.index ["station_end_id"], name: "index_lines_on_station_end_id"
     t.index ["station_start_id"], name: "index_lines_on_station_start_id"
   end
