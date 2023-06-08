242df503338a26562e050ae5adb0c1e4537f18d2
Vnt0n <antoine.chosson@gmail.com>
2023-06-08 10:34:21 +0200

    index

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -8,19 +8,6 @@ class JourneysController < ApplicationController
     @cities = City.all
   end
 
-    # if @search.present?
-    #   @name = @search["name"]
-    #   @querystation = Station.find_by(name: @name)
-    #   @selected_station = Station.where(["id = #{@querystation.id}"])
-    # end
-
-    # @markers = @selected_station.map do |station|
-    #   {
-    #     lat: station.latitude,
-    #     lng: station.longitude
-    #   }
-    # end
-
   def show
     @station = Station.find(params[:id])
     @journey = Journey.find(params[:id])
