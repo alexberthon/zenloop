9ca1869047474ad0ae5ac2592ef951248361ce88
Alexandre Berthon <alexandre_berthon@hotmail.fr>
2023-06-09 12:01:01 +0200

    Add Sidekiq & jobs/tasks to fetch and store data

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
   resources :profiles, only: [:show]
-  resources :cities
-  resources :stations
-
-  get "lines/search", to: "lines#search"
   resources :lines, only: [:index, :show]
-
   resources :journeys do
     resources :steps, only: [:create]
   end
   resources :steps, only: [:destroy]
+
+  mount Sidekiq::Web => '/sidekiq'
 end
