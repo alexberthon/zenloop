5170364547f1e86f3162e588593fa2f84794a60d
Alexandre Berthon <alexandre_berthon@hotmail.fr>
2023-06-13 12:34:36 +0200

    Add city photos in seeds

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,7 @@
 class City < ApplicationRecord
   has_many :activities
-  has_many :stations
-  has_many :stays
+  has_many :stations, dependent: :destroy
+  has_many :stays, dependent: :destroy
 
   has_one_attached :photo
   reverse_geocoded_by :latitude, :longitude
